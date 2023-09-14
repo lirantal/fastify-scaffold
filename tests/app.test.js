797c6b42362353d92b9ab2d8bc932bfa031b7e24
Liran Tal <liran.tal@gmail.com>
2023-09-14 10:24:57 +0300

    fix: get app.test.js to pass
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,17 +1,10 @@
 import test from "node:test";
 import assert from "node:assert";
 
-import AppFramework from "../app.js";
-
-import Fastify from "fastify";
-import fastifyPlugin from "fastify-plugin";
-
-import { config } from "../services/core/config.js";
+import { startApp, closeApp } from "../utils/tests/infra.js";
 
 test("AppFramework bootstraps successfully", async () => {
-  const app = Fastify();
-  app.register(fastifyPlugin(AppFramework), { config });
-  await app.ready();
+  const app = await startApp();
   assert.ok(app);
-  await app.close();
+  await closeApp(app);
 });
